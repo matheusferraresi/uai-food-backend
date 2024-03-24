376a64397cdac38cd1cc8e3e2c9913cf9f76c9e8
Marcos Theofilo <matheusferraresi@live.com>
2024-03-24 22:37:06 +0000

    graphql poc

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,20 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    console.log(createRestaurantInput);
+    return true;
+  }
+}
